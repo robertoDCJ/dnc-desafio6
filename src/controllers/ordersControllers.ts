5e5d944fd65a003b4ba4743b6f36e1b6a6ebe4e2
robertoDCJ <robertox70@hotmail.com>
2024-08-03 22:01:55 -0300

    refactored getOneOrder function

--- src/controllers/ordersControllers.ts
+++ src/controllers/ordersControllers.ts
@@ -1,15 +1,24 @@
 import { Request, Response } from "express";
+import { createClient } from "redis";
 import {
   createOrder,
   findAllOrdersOnStocks,
   findAllProducts,
   findManyOrders,
-  findManyOrdersOnStocks,
   findManyProducts,
-  findOrderById,
   getClientByNameOrId,
   getClients,
 } from "../utils";
+
+const client = createClient({
+  password: "wyCfJNs2GVmPqkjox3qz8bffj2vZZnzE",
+  socket: {
+    host: "redis-15074.c98.us-east-1-4.ec2.redns.redis-cloud.com",
+    port: 15074,
+  },
+})
+  .on("error", (err) => console.log("Redis Client Error", err))
+  .connect();
 
 //
 // ---------------- CREATE ORDER ----------------
@@ -48,6 +57,12 @@ export const postOrder = async (req: Request, res: Response) => {
 
 export const getAllOrders = async (req: Request, res: Response) => {
   try {
+    const clientInstance = await client;
+    const verifyOrders = await clientInstance.get("allOrders");
+    if (verifyOrders) {
+      return res.status(200).json(JSON.parse(verifyOrders));
+    }
+
     const [orders, ordersOnStocks, clients, products] = await Promise.all([
       findManyOrders(),
       findAllOrdersOnStocks(),
@@ -93,6 +108,7 @@ export const getAllOrders = async (req: Request, res: Response) => {
       })
       .filter((product) => product !== null);
 
+    clientInstance.set("allOrders", JSON.stringify(allOrders));
     res.status(200).json(allOrders);
   } catch (error) {
     res.status(400).json(error);
@@ -105,49 +121,34 @@ export const getAllOrders = async (req: Request, res: Response) => {
 
 export const getOneOrder = async (req: Request, res: Response) => {
   const { orderId } = req.params;
+  const clientInstance = await client;
 
   try {
-    const order = await findOrderById(Number(orderId));
-    if (order) {
-      const [client, orderOnStock] = await Promise.all([
-        getClientByNameOrId(order.cliente_id.toString()),
-        findManyOrdersOnStocks([order]),
-      ]);
-      if (client && orderOnStock) {
-        const productsId = orderOnStock
-          .flat()
-          .map((item) => ({ id: item.estoque_id }));
-        const products = await findManyProducts(productsId);
-
-        const productMap = new Map(
-          products.map((product) => [product.id, product])
-        );
-        const productWithQuantity = orderOnStock
-          .flat()
-          .map((item) => {
-            const product = productMap.get(item.estoque_id);
-            if (product) {
-              return {
-                id: product.id,
-                name: product.nome,
-                preco: product.preco,
-                quantity: item.quantidade,
-              };
-            }
-            return null;
-          })
-          .filter((product) => product !== null);
-
-        const orderResult = {
-          id: order.id,
-          client: client[0],
-          products: productWithQuantity,
-        };
-
-        return res.status(200).json(orderResult);
+    const jsonOrder = await clientInstance.get("oneOrder");
+    if (jsonOrder) {
+      const parseOrder = await JSON.parse(jsonOrder);
+      if (parseOrder.id === Number(orderId)) {
+        return res.status(200).json(parseOrder);
       }
     }
-    res.status(200).json(order);
+
+    const allOrders = await clientInstance.get("allOrders");
+    if (allOrders) {
+      const parseAllOrders: Array<{
+        id: number;
+        client: any;
+        products: any[];
+      }> = await JSON.parse(allOrders);
+      const orderMap = new Map(
+        parseAllOrders.map((order) => [order.id, order])
+      );
+
+      const order = orderMap.get(Number(orderId));
+      if (order) {
+        clientInstance.set("oneOrder", JSON.stringify(order));
+        return res.status(200).json(order);
+      }
+    }
   } catch (error) {
     res.status(400).json(error);
   }
